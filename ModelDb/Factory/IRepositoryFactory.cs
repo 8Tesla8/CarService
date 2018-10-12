80e11685bf84dfa08d60d37a23f13c1a61506c0d
tesla <kela256@gmail.com>
2018-10-12 23:41:51 +0300

    created RepositoryFactory

---
+++ ModelDb/Factory/IRepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ModelDb.Repository;
+
+namespace ModelDb.Factory
+{
+    public interface IRepositoryFactory
+    {
+        AppointmentRepository GetAppointmentRepository();
+        CarModelRepository GetModelRepository();
+        CarRepository GetCarRepository();
+        ServiceTypeRepository GetServiceType();
+        UserRepository GetUserRepository();
+    }
+}
---
+++ ModelDb/Factory/RepositoryFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ModelDb.Repository;
+
+namespace ModelDb.Factory
+{
+    public class RepositoryFactory
+    {
+        AppointmentRepository GetAppointmentRepository()
+        {
+            return new AppointmentRepository();
+        }
+
+        CarModelRepository GetModelRepository()
+        {
+            return new CarModelRepository();
+        }
+
+        CarRepository GetCarRepository()
+        {
+            return new CarRepository();
+        }
+
+        ServiceTypeRepository GetServiceType()
+        {
+            return new ServiceTypeRepository();
+        }
+
+        UserRepository GetUserRepository(){
+            return new UserRepository();
+        }
+    }
+}
